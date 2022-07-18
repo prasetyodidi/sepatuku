9c69061673caf2eee6b39649d357b1277fee5e6f
Didi Prasetyo <didiprasetyo360@gmail.com>
2022-07-18 20:08:31 +0700

    update shoes favorite fragment

---
+++ app/src/main/java/com/didi/sepatuku/presentation/shoe_favorite/FavoriteViewModel.kt
@@ -0,0 +1,71 @@
+package com.didi.sepatuku.presentation.shoe_favorite
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.didi.sepatuku.core.util.Resource
+import com.didi.sepatuku.domain.model.Shoe
+import com.didi.sepatuku.domain.use_case.FavoriteUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import timber.log.Timber
+import javax.inject.Inject
+
+data class FavoriteUIState(
+    val isLoading: Boolean = false,
+    val items: List<Shoe> = emptyList(),
+    val message: String? = null
+)
+
+@HiltViewModel
+class FavoriteViewModel @Inject constructor(
+    private val favoriteUseCase: FavoriteUseCase
+): ViewModel(){
+    private var _state: MutableStateFlow<FavoriteUIState> = MutableStateFlow(FavoriteUIState())
+    val state: StateFlow<FavoriteUIState> = _state.asStateFlow()
+
+    init {
+        Timber.d("fav view model")
+        favoriteUseCase.getFavorites()
+            .onEach { event ->
+                when(event){
+                    is Resource.Loading -> {
+                        Timber.d("fav view model loading : ${event.data?.size}")
+                        _state.value = state.value.copy(
+                            isLoading = true,
+                            items = event.data ?: emptyList()
+                        )
+                    }
+                    is Resource.Success -> {
+                        Timber.d("fav view model success : ${event.data?.size}")
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            items = event.data ?: emptyList()
+                        )
+                    }
+                    is Resource.Error -> {
+                        Timber.d("fav view model error : ${event.data?.size}")
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            message = event.message ?: "unknown error"
+                        )
+                    }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+
+//    var favoritesItems: LiveData<List<FavoriteEntity>> = favoriteRepository.favorites.asLiveData()
+//    private fun launchDataLoad(scope: CoroutineScope, block: suspend () -> Unit): Job{
+//        return scope.launch {
+//            try {
+//                _isLoading.value = true
+//                block
+//            } catch (e: Throwable) {
+//                _snackbar.value = e.message
+//            } finally {
+//                _isLoading.value = false
+//            }
+//        }
+//    }
+
+}
