84dee34eea7686cba3243b87a4823c305e8510c9
Didi Prasetyo <didiprasetyo360@gmail.com>
2022-07-18 20:07:33 +0700

    update detail shoe fragment

---
+++ app/src/main/java/com/didi/sepatuku/presentation/detail_shoe/DetailShoeViewModel.kt
@@ -0,0 +1,110 @@
+package com.didi.sepatuku.presentation.detail_shoe
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.didi.sepatuku.core.util.Resource
+import com.didi.sepatuku.data.local.entity.ShoeEntity
+import com.didi.sepatuku.data.local.entity.ShoppingCartEntity
+import com.didi.sepatuku.domain.model.CartItem
+import com.didi.sepatuku.domain.model.Shoe
+import com.didi.sepatuku.domain.use_case.FavoriteUseCase
+import com.didi.sepatuku.domain.use_case.ShoeUseCase
+import com.didi.sepatuku.domain.use_case.ShoppingCartUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import java.sql.SQLException
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailShoeViewModel @Inject constructor(
+    private val shoeUseCase: ShoeUseCase,
+    private val favoriteUseCase: FavoriteUseCase,
+    private val shoppingCartUseCase: ShoppingCartUseCase
+) : ViewModel() {
+
+    private val name: StateFlow<String> = MutableStateFlow("")
+
+    private var _state: MutableStateFlow<DetailShoeUIState> = MutableStateFlow(DetailShoeUIState())
+    val state: StateFlow<DetailShoeUIState> = _state.asStateFlow()
+
+    fun getDetailShoe(name: String) {
+        shoeUseCase.getDetailShoe(name)
+            .onEach { event ->
+                when (event) {
+                    is Resource.Loading -> {
+                        _state.value = state.value.copy(
+                            isLoading = true,
+                            detailShoe = event.data
+                        )
+                    }
+                    is Resource.Success -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            detailShoe = event.data
+                        )
+                    }
+                    is Resource.Error -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            detailShoe = event.data,
+                            message = event.message
+                        )
+                    }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+    fun changeFavorite(){
+        val value = !state.value.isFavorite
+        _state.value = state.value.copy(
+            isFavorite = value
+        )
+        if (value){
+            state.value.detailShoe?.let {
+                viewModelScope.launch {
+                    insertFavorite(it.intoShoe())
+                }
+            }
+        } else {
+            state.value.detailShoe?.let {
+                viewModelScope.launch {
+                    deleteFavorite(it.name)
+                }
+            }
+        }
+    }
+
+    private suspend fun insertFavorite(item: Shoe) {
+        try {
+            favoriteUseCase.insertFavorite(item)
+        } catch (e: SQLException) {
+            _state.value = state.value.copy(
+                message = e.message
+            )
+        }
+    }
+
+    private suspend fun deleteFavorite(name: String) {
+        try {
+            favoriteUseCase.deleteFavorite(name)
+        } catch (e: SQLException) {
+            _state.value = state.value.copy(
+                message = e.message
+            )
+        }
+    }
+
+    fun insertShoppingCart(item: ShoppingCartEntity) {
+        viewModelScope.launch {
+            try {
+                shoppingCartUseCase.insertShoppingCartItem(item)
+            } catch (e: SQLException) {
+                _state.value = state.value.copy(
+                    message = e.message
+                )
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/didi/sepatuku/presentation/detail_shoe/DetailShoesFragment.kt
@@ -0,0 +1,165 @@
+package com.didi.sepatuku.presentation.detail_shoe
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.bumptech.glide.Glide
+import com.didi.sepatuku.R
+import com.didi.sepatuku.databinding.FragmentDetailShoesBinding
+import com.didi.sepatuku.domain.model.DetailShoe
+import com.didi.sepatuku.presentation.shoe.HomeFragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+class DetailShoesFragment : Fragment() {
+    private var _binding: FragmentDetailShoesBinding? = null
+    private val binding get() = _binding
+    private val viewModel: DetailShoeViewModel by activityViewModels()
+    private lateinit var name: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val bundle = this.arguments
+
+        if (bundle != null) {
+            name = bundle.getString(HomeFragment.EXTRA_NAME).toString()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailShoesBinding.inflate(inflater, container, false)
+
+        viewModel.getDetailShoe(name)
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state
+                    .map { it.isLoading }
+                    .distinctUntilChanged()
+                    .collect {
+                        showLoading(it)
+                    }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state
+                    .map { it.detailShoe }
+                    .distinctUntilChanged()
+                    .collect {
+                        if (it != null) {
+                            showShoeInfo(it)
+                        }
+                    }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state
+                    .map { it.message }
+                    .distinctUntilChanged()
+                    .collect {
+                        it?.let {
+                            showSnackbar(it)
+                        }
+                    }
+            }
+        }
+
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.state
+                    .map { it.isFavorite }
+                    .distinctUntilChanged()
+                    .collect {
+                        showFavorite(it)
+                    }
+            }
+        }
+
+        binding?.btnAddItem?.setOnClickListener {
+            val dialog = DetailCartDialogFragment.newInstance()
+//            val behavior = (dialog.dialog as BottomSheetDialog).behavior
+//            behavior.saveFlags = BottomSheetBehavior.SAVE_PEEK_HEIGHT
+            dialog.show(parentFragmentManager, EXTRA_DIALOG)
+        }
+        binding?.btnFavorite?.setOnClickListener {
+            viewModel.changeFavorite()
+        }
+        binding?.btnShare?.setOnClickListener {
+            Timber.d("shared!")
+            Toast.makeText(activity, "Click icon Share ", Toast.LENGTH_SHORT).show()
+        }
+
+        return binding?.root
+    }
+
+    private fun showFavorite(state: Boolean) {
+        if (state) {
+            binding?.btnFavorite?.setImageResource(R.drawable.ic_baseline_favorite_24)
+        } else {
+            binding?.btnFavorite?.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+        }
+    }
+
+    private fun showShoeInfo(detailShoe: DetailShoe) {
+        binding?.run {
+            Glide.with(requireContext())
+                .load(detailShoe.imageUrl)
+                .into(imgItemPhotoDetail)
+
+            tvShoesPrice.text = "Rp. ${detailShoe.price}"
+            tvShoesName.text = detailShoe.name
+            tvShoesDesc.text = detailShoe.desc
+            Timber.d("sizes: ${detailShoe.sizes}")
+            tvShoesSizes.text = detailShoe.sizes.toString()
+            tvShoesStock.text = detailShoe.stock.toString()
+            showFavorite(detailShoe.isFavorite)
+        }
+    }
+
+    private fun showLoading(state: Boolean) {
+        if (state) {
+            binding?.progressBar?.visibility = View.VISIBLE
+        } else {
+            binding?.progressBar?.visibility = View.INVISIBLE
+        }
+    }
+
+    fun showSnackbar(message: String) {
+        view?.let { Snackbar.make(it, message, Snackbar.LENGTH_SHORT).show() }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = DetailShoesFragment()
+        const val EXTRA_DIALOG = "extra_dialog"
+    }
+}
+
+
