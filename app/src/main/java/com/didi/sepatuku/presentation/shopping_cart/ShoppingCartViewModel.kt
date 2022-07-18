aa95378c767654b3a65f3272b58eb5312bfd2a30
Didi Prasetyo <didiprasetyo360@gmail.com>
2022-07-18 20:13:03 +0700

    update shopping cart fragment

---
+++ app/src/main/java/com/didi/sepatuku/presentation/shopping_cart/ShoppingCartViewModel.kt
@@ -0,0 +1,81 @@
+package com.didi.sepatuku.presentation.shopping_cart
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.didi.sepatuku.core.util.Resource
+import com.didi.sepatuku.domain.model.CartItem
+import com.didi.sepatuku.domain.model.Shoe
+import com.didi.sepatuku.domain.use_case.ShoppingCartUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+data class ShoppingCartUiState(
+    val isLoading: Boolean = false,
+    val message: String? = null,
+    val items: List<CartItem> = emptyList()
+)
+
+@HiltViewModel
+class ShoppingCartViewModel @Inject constructor(
+    private val shoppingCartUseCase: ShoppingCartUseCase
+) : ViewModel() {
+
+    private var _state: MutableStateFlow<ShoppingCartUiState> =
+        MutableStateFlow(ShoppingCartUiState())
+    val state: StateFlow<ShoppingCartUiState> = _state.asStateFlow()
+
+    val stateTest: LiveData<Resource<List<CartItem>>> = shoppingCartUseCase.getShoppingCartItems().asLiveData()
+
+    init {
+        getData()
+    }
+
+    private fun getData(){
+        shoppingCartUseCase.getShoppingCartItems()
+            .onEach { event ->
+                when (event) {
+                    is Resource.Loading -> {
+                        _state.value = state.value.copy(
+                            isLoading = true,
+                            items = event.data ?: emptyList()
+                        )
+                    }
+                    is Resource.Success -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            items = event.data ?: emptyList()
+                        )
+                    }
+                    is Resource.Error -> {
+                        _state.value = state.value.copy(
+                            isLoading = false,
+                            message = event.message ?: "unknown error"
+                        )
+                    }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+    fun deleteItemFromShoppingCart(item: CartItem){
+        viewModelScope.launch {
+            Timber.d("viewModel noDelete:  ${item.name}")
+            shoppingCartUseCase.deleteShoppingCartItem(item)
+            getData()
+        }
+    }
+
+    fun updateAmount(item: CartItem, amount: Int){
+        viewModelScope.launch {
+            if (amount >= 1){
+                Timber.d("view model update amount ${item.amount} $amount")
+                shoppingCartUseCase.updateShoppingCartItem(item.copy(amount = amount))
+                getData()
+            }
+        }
+    }
+}
